82c7c2a104d263d2018b084db4191f6957c56888
EIM316 <eimemail2025@gmail.com>
2025-10-24 02:48:52 +0800

    Rename ProfessorWaitingRoom to Waitingroompage

--- src/app/student/play/classmode/waitingroom/page.tsx
+++ src/app/student/play/classmode/waitingroom/page.tsx
@@ -2,12 +2,12 @@
 "use client";
 
 import { Suspense } from "react";
-import ProfessorWaitingRoom from "./ProfessorWaitingRoom";
+import Waitingroompage from "./ProfessorWaitingRoom";
 
 export default function Page() {
   return (
     <Suspense fallback={<div className="flex items-center justify-center min-h-screen text-gray-700">Loading...</div>}>
-      <ProfessorWaitingRoom />
+      <Waitingroompage />
     </Suspense>
   );
 }
